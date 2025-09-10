18b4a3461b28a648ee6a47adef0acd2edc3c8d3b
Segueregis <105684859+Segueregis@users.noreply.github.com>
2025-09-09 21:47:43 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
-    allowedHosts: ["*"], // libera qualquer host, ideal para Replit
+    allowedHosts: [
+      "31289348-4f34-4c47-9b31-2088a083efbf-00-1j9un6v7sn3ol.picard.replit.dev",
+      "1e31e350-9163-4176-a655-68adf96638d6-00-3co9ozr6hd642.worf.replit.dev",
+      "f3bd62f6-ad03-4bd9-9252-0019803dee81-00-2wgqegqb8p41z.janeway.replit.dev"
+    ]
   },
   plugins: [
     react(),
